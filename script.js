aec1de689045231478ff8f6252a9a70d1ff8d84b
Julia Melik <julija.melik@gmail.com>
2022-08-02 09:12:52 +0200

    rok

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 console.log("Caligula");
 
 console.log( Math.floor(Math.random()) * 100 );
-
+/*
 let barva = prompt("Write color of yout hair");
 
 console.log("Color of my eyes is " + barva)
+*/
+
+let rok = prompt("Write year of your birth");
+let pocetRoku = 2022 - rok;
+
+console.log("User is " + pocetRoku + " old");
